b85824e991e5c6556ce6dd12a2f4036d9bac4501
Robert Sandoval <robsand1992@yahoo.com>
2021-10-07 13:30:27 -0700

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -30,12 +30,15 @@ var fight = function(enemyName) {
             window.alert(playerName + " has decided to skip this fight. Goodbye!")
         }
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
     }
-        // remove enemy's health by subtracting the amoutn set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        // generate random damgae value based on player's attack power
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage);
+
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
@@ -47,8 +50,10 @@ var fight = function(enemyName) {
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
 
-        // remove player's health by subtracting the amount set in the enemyATtack variable
-        playerHealth = playerHealth - enemyAttack;
+        // generate random damage value based on enemy's attack power
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
@@ -77,7 +82,7 @@ var startGame = function() {
 
     var pickedEnemyName = enemyNames[i];
     // call fight funciton with enemy-robot
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40, 60);
 
     fight(pickedEnemyName);
 
@@ -169,6 +174,13 @@ var shop = function() {
             shop();
             break;
     }
+};
+
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
 };
 
 
